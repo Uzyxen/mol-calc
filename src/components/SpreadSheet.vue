b1903a9d6e1d6dab0478973346fde5905ef40010
Uzyxen <michal912456@gmail.com>
2023-06-11 15:17:20 +0200

    binding values from inputs

--- src/components/SpreadSheet.vue
+++ src/components/SpreadSheet.vue
@@ -8,6 +8,12 @@
     <div id="rows">
       <div class="row" v-for="row in 100" :key="row">{{ row }}</div>
     </div>
+
+    <div id="sheet">
+      <div class="sheet-row" v-for="row in 100" :key="row">
+        <input type="text" class="sheet-column" v-for="column in columns" :key="column" :id="column + row" v-model="inputValues[column + row]">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,8 +24,14 @@
         columns: [
           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
           'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-        ]
+        ],
+
+        inputValues: {}
       }
+    },
+
+    methods:{
+
     }
   }
 </script>
@@ -34,6 +46,7 @@
     display: flex;
     position: sticky;
     top: 0;
+    width: 300vh;
   }
 
   #columns div:not(:nth-child(2)){
@@ -50,14 +63,39 @@
     width: 70px;
     height: 20px;
     border: 1px solid #aaa;
+    border-top: none;
   }
 
   .row{
     display: flex;
     justify-content: center;
-    width: 49px;
+    width: 50px;
     height: 20px;
     border: 1px solid #aaa;
     border-left: none;
+  }
+
+  #sheet{
+    position: absolute;
+    top: 22px;
+    left: 51px;
+    width: calc(100% - 50px);
+    height: 250vh;
+  }
+
+  .sheet-row{
+    display: flex;
+    width: 100%;
+    height: 21px;
+  }
+
+  .sheet-column{
+    border: 1px solid #aaa;
+    border-left: none;
+    border-top: none;
+    outline: none;
+    padding: 0;
+    width: 70px;
+    height: 20px;
   }
 </style>
