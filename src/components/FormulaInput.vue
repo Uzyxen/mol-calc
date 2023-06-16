c177a6aa3025cbb5da9448cbfee5ba2d1824ad92
Uzyxen <michal912456@gmail.com>
2023-06-16 12:54:38 +0200

    formula input as a separate component

---
+++ src/components/FormulaInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="functions">
+        <h2>=</h2>
+        <input type="text" id="formula" autocomplete="off" v-bind:value="formula">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['formula']
+    }
+</script>
+
+<style>
+
+</style>
